adf1fb5a4a19f66eb202a33d4478fb379463babd
Aaron Brown <snakeskipper@proton.me>
2023-02-08 18:44:32 -0500

    made a test for funcdef

--- testcases.py
+++ testcases.py
@@ -178,6 +178,9 @@ TEST_CASES = [
     ('int main() {\n\tint testint = 0;\n\ttestint = 30;\n\treturn 0;\n}', 'testint : NUMERUS MUTABILIS testint .III.NIL. VALORUM', 'main'),
 
     # funcdef <-- todo last
+    ('void countdown(int start, char* message) {\n\tintcurrent = 0;\n\tcurrent = start;\n\t while (current > 0) {\n\t\t current = displayanddecrement(current);\n\t}\n\tprintf("%s", __throbac_cat(message, "\n"));\n}',
+     'APUD start: NUMERUS, message : LOCUTIO DEFINITIO countdown > current : NUMERUS MUTABILIS current start VALORUM current SUPRA .NIL. DUM > current APUD current VOCO displayanddecrement VALORUM < message IUNGO ^+^ LOCUTIO.IMPRIMO <'
+     'funcdef')
     # script
 ]
 
